433d2bc7bfc417877af349a2727c352a357ca37c
lgConrado <lgconrado.lgc@gmail.com>
2024-12-18 09:42:37 -0300

    Criação estrutura de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Rotas from "./routes";
+import './styles/style.scss';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <h1>Ola mundo</h1>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <Rotas />
+    </StrictMode>
+  </BrowserRouter>
+);
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route path="/" element={< h1/>} />
+    </Routes>
+  );
+};
+
+export default Rotas;
